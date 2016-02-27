61dc02802647de6ff86a5cf185f5299cb9c9fa21
kneekey23 <nicoleklein@me.com>
2016-02-27 13:38:33 -0800

    create account page

---
+++ Betagig/CompanyCreateAccountController.swift
@@ -0,0 +1,13 @@
+//
+//  CompanyCreateAccountController.swift
+//  Betagig
+//
+//  Created by Nicki on 2/27/16.
+//  Copyright © 2016 shortkey. All rights reserved.
+//
+
+import UIKit
+
+class CompanyCreateAccountController: UIViewController {
+
+}
