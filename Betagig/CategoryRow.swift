a346f22dcbe7e1b3e78a750929985aa5bd5d5893
kneekey23 <nicoleklein@me.com>
2016-02-26 18:55:28 -0800

    grid view

---
+++ Betagig/CategoryRow.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryRow.swift
+//  Betagig
+//
+//  Created by Nicki on 2/26/16.
+//  Copyright © 2016 shortkey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategoryRow : UITableViewCell {
+
+}
+
+extension CategoryRow : UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("careerCell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.pinImage.image = UIImage(named: "Cook")
+        return cell
+    }
+    
+}
+
+extension CategoryRow : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let itemsPerRow:CGFloat = 4
+        let hardCodedPadding:CGFloat = 5
+        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+}
---
+++ Betagig/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  Betagig
+//
+//  Created by Nicki on 2/26/16.
+//  Copyright © 2016 shortkey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+
+    @IBOutlet weak var pinImage: UIImageView!
+ 
+    @IBOutlet weak var title: UILabel!
+}
