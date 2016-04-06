1b4eaec37e2355cf0fe7912a547b3c12c8791e11
Melissa Hargis <melissa@shortkey.io>
2016-04-06 14:45:21 -0700

    minor color tweak

---
+++ Betagig/MessagesController.swift
@@ -0,0 +1,28 @@
+//
+//  MessagesController.swift
+//  Betagig
+//
+//  Created by Melissa Hargis on 4/6/16.
+//  Copyright © 2016 shortkey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MessagesController: UIViewController {
+    
+    @IBOutlet weak var closeBtn: UIButton!
+
+//    var delegate:MessagesViewControllerDelegate!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+//protocol MessagesViewControllerDelegate
+//{
+//    func sendValue(var value : NSString)
+//}
